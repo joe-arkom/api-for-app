9e6e937e3907cd8c351a33a29d64f2383934479c
joe-arkom <joseph.allen@arkom.co.uk>
2023-10-22 21:52:11 +0100

    "Refactor connection string to reference appsettings"

--- Program.cs
+++ Program.cs
@@ -1,3 +1,7 @@
+using Microsoft.EntityFrameworkCore;
+using Serilog;
+using web_api.Entities;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +10,23 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<AppDatabaseContext>(options =>
+{
+    var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+    if (connectionString != null)
+    {
+        options.UseMySQL(connectionString);
+    }
+    else
+    {
+        // Log an error when the connection string is not found.
+        Log.Error("Connection string 'DefaultConnection' not found in appsettings.json");
+        // You can also throw an exception or handle the case as needed.
+    }
+});
+
 
 var app = builder.Build();
 
